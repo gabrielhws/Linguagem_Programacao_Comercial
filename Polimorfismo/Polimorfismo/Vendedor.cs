43c68ce6eada66a68271b3fea541b63f565c2e38
Glaydston Veloso <glaydston.oliveira@gmail.com>
2013-04-18 09:36:22 -0300

    Initial Commit

---
+++ Polimorfismo/Polimorfismo/Vendedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Vendedor : Pessoa
+    {
+        public void Viajar()
+        {
+            Console.WriteLine("Viajar em Vendedor");
+        }
+    }
+}
